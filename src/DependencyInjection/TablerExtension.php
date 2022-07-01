9ab3772989e7659c0a6f9f90381e7bc5cc8e2d71
Kevin Papst <kevinpapst@users.noreply.github.com>
2022-07-01 23:22:01 +0200

    removed unused and some very opinionated code from the forms theme (#97)

--- src/DependencyInjection/TablerExtension.php
+++ src/DependencyInjection/TablerExtension.php
@@ -66,25 +66,5 @@ class TablerExtension extends Extension implements PrependExtensionInterface
         $container->setParameter('tabler_bundle.options', $options);
         $container->setParameter('tabler_bundle.routes', $routes);
         $container->setParameter('tabler_bundle.icons', $icons);
-
-        if (!\array_key_exists('form_theme', $options) || null === ($theme = $options['form_theme'])) {
-            return;
-        }
-
-        $themes = [
-            'default' => '@Tabler/layout/form-theme.html.twig',
-            'horizontal' => '@Tabler/layout/form-theme-horizontal.html.twig',
-        ];
-
-        if (!\array_key_exists($theme, $themes)) {
-            return;
-        }
-
-        $bundles = $container->getParameter('kernel.bundles');
-
-        // register the form theme
-        if (isset($bundles['TwigBundle'])) {
-            $container->prependExtensionConfig('twig', ['form_theme' => [$themes[$theme]]]);
-        }
     }
 }
