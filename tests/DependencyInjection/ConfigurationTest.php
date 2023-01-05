78d7a9d99f1a096bad82ca9662914e968eff6f59
Cavasin F <cavasinf.info@gmail.com>
2023-01-05 14:40:26 +0100

    Revert delete config option

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -72,6 +72,7 @@ class ConfigurationTest extends TestCase
                     'tabler_registration' => null,
                     'tabler_registration_register' => null,
                     'tabler_password_reset' => null,
+                    'tabler_password_reset_sent' => null,
                 ],
                 'icons' => [],
             ]
