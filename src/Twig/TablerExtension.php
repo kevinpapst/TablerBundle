b680240ad679b765e216c3bdb1f1f9f464be7213
Kevin Papst <kpapst@gmx.net>
2023-05-14 16:58:50 +0200

    new theme handling

--- src/Twig/TablerExtension.php
+++ src/Twig/TablerExtension.php
@@ -23,7 +23,6 @@ class TablerExtension extends AbstractExtension
         return [
             new TwigFilter('tabler_container', [RuntimeExtension::class, 'containerClass']),
             new TwigFilter('tabler_body', [RuntimeExtension::class, 'bodyClass']),
-            new TwigFilter('tabler_theme', [RuntimeExtension::class, 'theme']),
             new TwigFilter('tabler_route', [RuntimeExtension::class, 'getRouteByAlias']),
             new TwigFilter('tabler_icon', [RuntimeExtension::class, 'icon']),
         ];
@@ -35,11 +34,12 @@ class TablerExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
+            new TwigFunction('tabler_icon', [RuntimeExtension::class, 'createIcon'], ['is_safe' => ['html']]),
             new TwigFunction('tabler_menu', [RuntimeExtension::class, 'getMenu']),
             new TwigFunction('tabler_notifications', [RuntimeExtension::class, 'getNotifications']),
-            new TwigFunction('tabler_user', [RuntimeExtension::class, 'getUserDetails']),
-            new TwigFunction('tabler_icon', [RuntimeExtension::class, 'createIcon'], ['is_safe' => ['html']]),
+            new TwigFunction('tabler_theme', [RuntimeExtension::class, 'theme']),
             new TwigFunction('tabler_unique_id', [RuntimeExtension::class, 'uniqueId']),
+            new TwigFunction('tabler_user', [RuntimeExtension::class, 'getUserDetails']),
         ];
     }
 }
