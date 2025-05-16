a1b09b766a4bfd909217db83cab7a794b25d2cb6
Emanuele Panzeri <thepanz@gmail.com>
2025-05-16 16:21:41 +0200

    Use non-deprecated Extension class (#220)

--- src/DependencyInjection/TablerExtension.php
+++ src/DependencyInjection/TablerExtension.php
@@ -11,9 +11,9 @@ namespace KevinPapst\TablerBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class TablerExtension extends Extension implements PrependExtensionInterface
 {
