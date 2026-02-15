7644ca4a6816fc6a20bd747de89dbf6a1987c0ee
Kevin Papst <kevin@kevinpapst.de>
2026-02-15 09:18:11 +0100

    bundle extension was using prepend to attach its own configuration, use container build instead

--- src/DependencyInjection/TablerExtension.php
+++ src/DependencyInjection/TablerExtension.php
@@ -12,10 +12,9 @@ namespace KevinPapst\TablerBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 
-class TablerExtension extends Extension implements PrependExtensionInterface
+class TablerExtension extends Extension
 {
     /**
      * {@inheritdoc}
@@ -24,47 +23,22 @@ class TablerExtension extends Extension implements PrependExtensionInterface
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        $options = $this->getContextOptions($config);
-
-        if (\count($options) > 0) {
-            $container->setParameter('tabler_bundle.options', $options);
-        }
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
-        $loader->load('services.yml');
-
-        if (\array_key_exists('knp_menu', $options) && \array_key_exists('enable', $options['knp_menu']) && $options['knp_menu']['enable'] === true) {
-            $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config/container'));
-            $loader->load('knp-menu.yml');
-        }
-    }
-
-    /**
-     * Merge available configuration options, so they are all available for the ContextHelper.
-     *
-     * @param array<string, mixed> $config
-     * @return array<string, array<string, mixed>>
-     */
-    private function getContextOptions(array $config): array
-    {
-        $contextOptions = (array) ($config['options'] ?? []);
-        $contextOptions['knp_menu'] = (array) ($config['knp_menu'] ?? []);
-
-        return $contextOptions;
-    }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        $configuration = new Configuration();
-        $configs = $container->getExtensionConfig($this->getAlias());
-        $config = $this->processConfiguration($configuration, $configs);
 
         $options = (array) ($config['options'] ?? []);
+        $options['knp_menu'] = (array) ($config['knp_menu'] ?? []);
         $routes = (array) ($config['routes'] ?? []);
         $icons = (array) ($config['icons'] ?? []);
 
         $container->setParameter('tabler_bundle.options', $options);
         $container->setParameter('tabler_bundle.routes', $routes);
         $container->setParameter('tabler_bundle.icons', $icons);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.yml');
+
+        if (\array_key_exists('enable', $options['knp_menu']) && $options['knp_menu']['enable'] === true) {
+            $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config/container'));
+            $loader->load('knp-menu.yml');
+        }
     }
 }
